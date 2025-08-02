47f0638a89402d7d2de64d2cf1e56f7fe65085b0
Egor Pushkart <egorpushkart@Mac-mini-Egor.local>
2025-08-02 04:37:38 -0700

    initial commit

---
+++ src/main/java/com/example/springaidemonoreact/SpringAiDemoNoReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.springaidemonoreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiDemoNoReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiDemoNoReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springaidemonoreact/SpringAiDemoNoReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springaidemonoreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiDemoNoReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
