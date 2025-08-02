355cef35ed62bf9dcccf57f8c68c59000f8080cd
Egor Pushkart <egorpushkart@Mac-mini-Egor.local>
2025-08-02 06:53:45 -0700

    add docker compose

--- src/main/java/com/example/springaidemonoreact/SpringAiDemoNoReactApplication.java
+++ src/main/java/com/example/springaidemonoreact/SpringAiDemoNoReactApplication.java
@@ -1,13 +1,23 @@
 package com.example.springaidemonoreact;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringAiDemoNoReactApplication {
 
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder builder) {
+        return builder.build();
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringAiDemoNoReactApplication.class, args);
+        var context = SpringApplication.run(SpringAiDemoNoReactApplication.class, args);
+        var chatClient = context.getBean(ChatClient.class);
+        System.out.println(chatClient.prompt().user("Напиши текст песни Queen Багемская рапсодия").call().content());
     }
 
 }
